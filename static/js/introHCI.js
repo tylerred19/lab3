ef34eec8b874f271400109b6d4c29dc8bc4dcdf0
Tam Tran <ttt016@ucsd.edu>
2021-01-20 11:27:25 -0800

    update lab 3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,34 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript has taken control");
+		// $("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+
+	$("a.thumbnail").click(projectClick);
+}
+
+// projectClick function
+
+function projectClick(e) { 
+
+	// prevent the page from reloading  
+	e.preventDefault();
+
+	// var containingProject = $(this).closest(".project");
+	// containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+	
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+	   // description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+	  $(".project-description").fadeToggle();
+    }
+
 }
